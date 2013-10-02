da48b83dcd96cb219228941bc591ec2dc42e57cf
resteve <resteve@zikzakmedia.com>
2013-10-02 17:53:28 +0200

    Header py files

--- tests/test_account_financial_statement_es.py
+++ tests/test_account_financial_statement_es.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
-# The COPYRIGHT file at the top level of this repository contains the full
-# copyright notices and license terms.
+#This file is part account_financial_statement_es module for Tryton.
+#The COPYRIGHT file at the top level of this repository contains
+#the full copyright notices and license terms.
 
 import sys
 import os
