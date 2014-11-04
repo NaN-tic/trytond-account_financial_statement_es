e61c800ef17bf5c4886db29cf0355056417f1dc7
Sergi Almacellas Abellana <sergi@koolpi.com>
2014-11-04 12:52:54 +0100

    Migrate to version 3.4

--- tests/test_account_financial_statement_es.py
+++ tests/test_account_financial_statement_es.py
@@ -1,17 +1,8 @@
 #!/usr/bin/env python
-#This file is part account_financial_statement_es module for Tryton.
-#The COPYRIGHT file at the top level of this repository contains
-#the full copyright notices and license terms.
-
-import sys
-import os
-DIR = os.path.abspath(os.path.normpath(os.path.join(__file__,
-    '..', '..', '..', '..', '..', 'trytond')))
-if os.path.isdir(DIR):
-    sys.path.insert(0, os.path.dirname(DIR))
-
+# This file is part account_financial_statement_es module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains
+# the full copyright notices and license terms.
 import unittest
-#import doctest TODO: Remove if no sceneario needed.
 import trytond.tests.test_tryton
 from trytond.tests.test_tryton import test_depends
 
@@ -22,12 +13,11 @@ class AccountFinancialStatementEsTestCase(unittest.TestCase):
     '''
 
     def setUp(self):
-        trytond.tests.test_tryton.install_module('account_financial_statement_es')
+        trytond.tests.test_tryton.install_module(
+            'account_financial_statement_es')
 
     def test0006depends(self):
-        '''
-        Test depends.
-        '''
+        'Test depends'
         test_depends()
 
 
@@ -36,6 +26,3 @@ def suite():
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
         AccountFinancialStatementEsTestCase))
     return suite
-
-if __name__ == '__main__':
-    unittest.TextTestRunner(verbosity=2).run(suite())
