f2607f8a3343ba7dce732011bb84419c4aafe660
resteve <resteve@zikzakmedia.com>
2015-12-01 10:56:33 +0100

    Tests

--- tests/test_account_financial_statement_es.py
+++ tests/test_account_financial_statement_es.py
@@ -1,24 +1,14 @@
-#!/usr/bin/env python
-# This file is part account_financial_statement_es module for Tryton.
-# The COPYRIGHT file at the top level of this repository contains
-# the full copyright notices and license terms.
+# This file is part of the account_financial_statement_es module for Tryton.
+# The COPYRIGHT file at the top level of this repository contains the full
+# copyright notices and license terms.
 import unittest
 import trytond.tests.test_tryton
-from trytond.tests.test_tryton import test_depends
+from trytond.tests.test_tryton import ModuleTestCase
 
 
-class AccountFinancialStatementEsTestCase(unittest.TestCase):
-    '''
-    Test Account Financial Statement Es module.
-    '''
-
-    def setUp(self):
-        trytond.tests.test_tryton.install_module(
-            'account_financial_statement_es')
-
-    def test0006depends(self):
-        'Test depends'
-        test_depends()
+class AccountFinancialStatementEsTestCase(ModuleTestCase):
+    'Test Account Financial Statement Es module'
+    module = 'account_financial_statement_es'
 
 
 def suite():
