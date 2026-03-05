a656cb7a360e0676a644ec0cad120c9de90ea4a5
Hector Rodriguez Olmo <hector.rodriguez@nan-tic.com>
2026-03-05 11:51:39 +0100

    Add financial statement templates and update balance reports with date fields | #185665

--- __init__.py
+++ __init__.py
@@ -2,6 +2,12 @@
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
 
+from trytond.pool import Pool
+
+from . import report
+
 
 def register():
-    pass
+    Pool.register(
+        report.FinancialStatementTemplate,
+        module='account_financial_statement_es', type_='model')
